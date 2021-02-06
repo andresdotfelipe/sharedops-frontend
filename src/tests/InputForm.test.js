5cb6bbf94504e7a685b6c8134aeaf8044f60fd43
andresfelipedev <andresfelipedev@gmail.com>
2021-02-06 11:24:48 -0500

    Update InputForm test

--- src/tests/InputForm.test.js
+++ src/tests/InputForm.test.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import InputForm from '../components/InputForm';
 
-describe('InputForm component', () => {    
-    
-    const defaultProps = {
-        meta: {
-            error: false,
-            touched: false
-        },
-        input: {
-            name: 'name',
-            value: 'value',
-            onChange: jest.fn(),
-            onBlur: jest.fn()
-        },
-        label: 'label',                
-        disabled: false
-    }    
+const onChange = jest.fn();
+const adaptFileEventToValue = jest.fn();
+const defaultProps = {
+    meta: {
+        error: '',
+        touched: false
+    },
+    input: {
+        name: 'name',        
+        onChange,
+        onBlur: jest.fn()
+    },
+    label: 'label',                
+    disabled: false
+};
 
-    it('renders correctly when input is text', () => {
+describe('InputForm component', () => {
+    
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('updates input text value when changed', () => {
         const inputTextProps = {
             ...defaultProps, 
             type: 'text',
             ph: 'placeholder'            
-        }
-        const tree = renderer.create(<InputForm {...inputTextProps} />).toJSON();
-        expect(tree).toMatchSnapshot();
+        };
+        const wrapper = shallow(<InputForm {...inputTextProps} />);
+        const event = { target: { value: 'Test value' } };
+        wrapper.find('FormControl').simulate('change', event);
+        expect(onChange).toHaveBeenCalledWith(event);
     });
 
     it('renders correctly when input is textarea', () => {
@@ -35,17 +42,28 @@ describe('InputForm component', () => {
             as: 'textarea',
             rows: 10,
             ph: 'placeholder'            
-        }
-        const tree = renderer.create(<InputForm {...inputTextAreaProps} />).toJSON();
-        expect(tree).toMatchSnapshot();
+        };
+        const wrapper = shallow(<InputForm {...inputTextAreaProps} />);        
+        const event = { target: { value: 'Test value' } };
+        wrapper.find('FormControl').simulate('change', event);
+        expect(onChange).toHaveBeenCalledWith(event);
+        
     });
 
     it('renders correctly when input is file', () => {
-        const inputFileProps = {
-            ...defaultProps, 
-            type: 'file'            
-        }
-        const tree = renderer.create(<InputForm {...inputFileProps} />).toJSON();
-        expect(tree).toMatchSnapshot();
+        try {            
+            defaultProps.onChange = adaptFileEventToValue(onChange);        
+            const inputFileProps = {
+                ...defaultProps, 
+                type: 'file'            
+            };
+            const wrapper = shallow(<InputForm {...inputFileProps} />);            
+            const file =  new File([new ArrayBuffer(1)], 'file.jpg');        
+            const event = { target: { files: { 0: file, length: 1 } } };         
+            wrapper.find('FormControl').simulate('change', event);
+            expect(adaptFileEventToValue).toHaveBeenCalled();
+        } catch (e) {
+            console.log(e);
+        }        
     });
 });
