39be27adeef7d48081c68c1d7ad4b88bee82a4a0
andresfelipedev <andresfelipedev@gmail.com>
2021-03-06 12:29:42 -0500

    Add OpinionsContainer and its props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
-import React, { Fragment, useState, useEffect, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory, Link } from 'react-router-dom';
-import { Button, Col, Container, Row, Spinner } from 'react-bootstrap';
-import { updateUserFavoriteOpinions } from '../actions/users';
-import { setOpinion, getOpinions } from '../actions/opinions';
+import React, { Fragment, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import OpinionsContainer from '../components/OpinionsContainer';
 
 const Home = () => {    
 
@@ -13,177 +10,29 @@ const Home = () => {
         window.scrollTo(0, 0);
     };    
 
-    const [isFetching, setIsFetching] = useState(false);
-    const [isHalfPage, setIsHalfPage] = useState(false);
-    const history = useHistory();
-
-    const { session, darkTheme, user, opinions, opinionPageCount, totalOpinionCount, currentOpinionCount } = useSelector(state => ({
-        session: state.UserReducer.session,
-        darkTheme: state.UserReducer.darkTheme,
-        user: state.UserReducer.user,
+    const { opinions, opinionPageCount, totalOpinionCount, currentOpinionCount } = useSelector(state => ({                        
         opinions: state.OpinionReducer.opinions,
         opinionPageCount: state.OpinionReducer.opinionPageCount,
         totalOpinionCount: state.OpinionReducer.totalOpinionCount,
         currentOpinionCount: state.OpinionReducer.currentOpinionCount,
-    }));    
-
-    const dispatch = useDispatch();    
-
-    const loadOnScroll = useCallback(
-        e => {
-            window.pageYOffset > 3572 ? setIsHalfPage(true) : setIsHalfPage(false);
-            if (currentOpinionCount === totalOpinionCount) return;            
-            const opinionsEnd = document.getElementById('opinions-end');            
-            const rect = opinionsEnd.getBoundingClientRect();
-            const isAtEnd = (
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
-            );            
-            if (isAtEnd) {                
-                if (isFetching) return;                  
-                setIsFetching(true);                
-                dispatch(getOpinions(`page=${opinionPageCount}`));
-                setIsFetching(false);
-            }            
-        },
-        [dispatch, totalOpinionCount, currentOpinionCount, isFetching, opinionPageCount]
-    );
-
-    const handleClickOpinion = opinion => {
-        dispatch(setOpinion(opinion));
-        history.push(`/comments/${opinion._id}/${opinion.title}`);
-    };
-
-    const handleAddFavorite = (e, opinion) => {
-        e.stopPropagation();
-        !session ? window.location = `/signin` : dispatch(updateUserFavoriteOpinions(opinion._id));        
-    };
+    }));
     
-    const handleBackToTop = () => {
-        window.scrollTo(0, 0);
+    const props = {
+        initialMessage: 'Sharedops helps you to share what you think, just like other people have done below.',
+        opinions,
+        opinionPageCount,
+        totalOpinionCount,
+        currentOpinionCount
     };
 
     useEffect(() => {        
         window.scrollTo(0, 0);
     }, []);
 
-    useEffect(() => {               
-        if (opinions.length === 0) {            
-            dispatch(getOpinions(`page=${opinionPageCount}`));            
-            //setIsFetching(false);                                  
-        } else {                   
-            window.addEventListener('scroll', loadOnScroll);
-        }                       
-        return () => {            
-            window.removeEventListener('scroll', loadOnScroll);            
-        };
-    }, [dispatch, opinions, opinionPageCount, loadOnScroll]);    
-
     return (
         <Fragment>
             <Header />
-            <Container className="home">
-                <Row className={`${darkTheme ? 'dark' : 'light'}`}>
-                    <Col xs={12} className="initial-message">
-                        <span>Sharedops helps you to share what you think, just like other people have done below.</span>
-                    </Col>
-                    {
-                        opinions ?
-                        <Row className="opinions-grid">
-                            {
-                                opinions.map((opinion, index) => {
-                                    return (
-                                        <Col 
-                                            key={opinion._id} 
-                                            xs={12} tabIndex={'0'} 
-                                            className="opinion-box"
-                                            onClick={() => handleClickOpinion(opinion)}>
-                                            <Row>
-                                                <Col xs={12} className="details">
-                                                    <span className="name">
-                                                        Opinion by <Link 
-                                                            to="/signup" 
-                                                            onClick={e => e.stopPropagation()}>
-                                                                {opinion.author.name}
-                                                                <img 
-                                                                    src={opinion.author.profilePicUrl}                                                    
-                                                                    alt={opinion.author.name}
-                                                                    className="profile-pic"
-                                                                /> 
-                                                        </Link>
-                                                    </span>                                                                                                       
-                                                    <span className="date">
-                                                        on {opinion.createdAt}
-                                                    </span>
-                                                </Col>
-                                                <Col xs={12} className="title">
-                                                    <Link to={`/comments/${opinion._id}/${opinion.title}`}>
-                                                        {opinion.title}
-                                                    </Link>
-                                                </Col>
-                                                {
-                                                    opinion.opinionImageUrl &&
-                                                    <Col xs={12}>
-                                                        <img 
-                                                            src={opinion.opinionImageUrl}
-                                                            alt={opinion.title}
-                                                            className="opinion-img"
-                                                        />
-                                                    </Col>
-                                                }                                                
-                                                <Col xs={12} className="body">
-                                                    {opinion.body}
-                                                </Col>
-                                                <Col xs={12} className="options">
-                                                    {
-                                                        user ?
-                                                        <Button 
-                                                            className={`${user.favoriteOpinions.some(e => e === opinion._id) ? 
-                                                                'favorite-checked' : 'favorite-unchecked'}`
-                                                            }
-                                                            onClick={e => handleAddFavorite(e, opinion)}>
-                                                            <i className="fas fa-star"></i>
-                                                        </Button> :
-                                                        <Button 
-                                                            className="favorite-unchecked"
-                                                            onClick={handleAddFavorite}>
-                                                            <i className="fas fa-star"></i>
-                                                        </Button>
-                                                    }                                                    
-                                                    <Button className="comment">
-                                                        <i className="fas fa-comments"></i>
-                                                    </Button>
-                                                </Col>                                            
-                                            </Row>
-                                        </Col>
-                                    );
-                                })
-                            }
-                        </Row> :
-                        <Col xs={12} className="resource-not-found">
-                            <span>No opinions</span>
-                        </Col>
-
-                    }
-                    {
-                        (currentOpinionCount !== totalOpinionCount) ?
-                        <Col xs={12} id="opinions-end">
-                            <Spinner
-                                as="span"
-                                animation="grow"                        
-                                role="status"
-                                aria-hidden="true"
-                            />
-                        </Col> : null
-                    }
-                    {
-                        isHalfPage &&
-                        <Col xs={12} className="up-arrow">                        
-                            <i className="fas fa-arrow-circle-up" onClick={handleBackToTop}></i>                    
-                        </Col>
-                    }                                     
-                </Row>                
-            </Container>
+            <OpinionsContainer {...props} />
             <Footer />
         </Fragment>
     );
