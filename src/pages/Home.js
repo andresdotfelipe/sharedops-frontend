76762892f9bb52c95b2bc2712bf6fdc61f77e324
andresfelipedev <andresfelipedev@gmail.com>
2021-03-06 19:48:22 -0500

    Add All Opinions props to OpinionsContainer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,28 +6,21 @@ import OpinionsContainer from '../components/OpinionsContainer';
 
 const Home = () => {    
 
-    window.onunload = () => {
-        window.scrollTo(0, 0);
-    };    
-
-    const { opinions, opinionPageCount, totalOpinionCount, currentOpinionCount } = useSelector(state => ({                        
-        opinions: state.OpinionReducer.opinions,
-        opinionPageCount: state.OpinionReducer.opinionPageCount,
-        totalOpinionCount: state.OpinionReducer.totalOpinionCount,
-        currentOpinionCount: state.OpinionReducer.currentOpinionCount,
+    const { allOpinions, allOpinionsPageCount, allOpinionsCurrentCount, allOpinionsTotalCount } = useSelector(state => ({                        
+        allOpinions: state.OpinionReducer.allOpinions,
+        allOpinionsPageCount: state.OpinionReducer.allOpinionsPageCount,
+        allOpinionsCurrentCount: state.OpinionReducer.allOpinionsCurrentCount,
+        allOpinionsTotalCount: state.OpinionReducer.allOpinionsTotalCount,
     }));
     
     const props = {
         initialMessage: 'Sharedops helps you to share what you think, just like other people have done below.',
-        opinions,
-        opinionPageCount,
-        totalOpinionCount,
-        currentOpinionCount
+        opinions: allOpinions,
+        pageCount: allOpinionsPageCount,
+        currentCount: allOpinionsCurrentCount,
+        totalCount: allOpinionsTotalCount,
+        type: 'allOpinions'
     };
-
-    useEffect(() => {        
-        window.scrollTo(0, 0);
-    }, []);
 
     return (
         <Fragment>
