8a1bd6a49e6724646758275d4840257ca5b1006e
andresfelipedev <andresfelipedev@gmail.com>
2021-03-08 11:15:59 -0500

    Add MyOpinions.js

---
+++ src/pages/MyOpinions.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import OpinionsContainer from '../components/OpinionsContainer';
+
+const MyOpinions = () => {    
+
+    const { session, myOpinions, myOpinionsPageCount, myOpinionsCurrentCount, myOpinionsTotalCount } = useSelector(state => ({
+        session: state.UserReducer.session,
+        myOpinions: state.OpinionReducer.myOpinions,
+        myOpinionsPageCount: state.OpinionReducer.myOpinionsPageCount,
+        myOpinionsCurrentCount: state.OpinionReducer.myOpinionsCurrentCount,
+        myOpinionsTotalCount: state.OpinionReducer.myOpinionsTotalCount
+    }));
+    
+    const props = {
+        initialMessage: 'These are your opinions, the ones you have shared with the world.',
+        opinions: myOpinions,
+        pageCount: myOpinionsPageCount,
+        currentCount: myOpinionsCurrentCount,
+        totalCount: myOpinionsTotalCount,
+        type: 'myOpinions'
+    };
+
+    if (!session) return <Redirect to={{ pathname: '/' }} />
+
+    return (
+        <Fragment>
+            <Header />
+            <OpinionsContainer {...props} />
+            <Footer />
+        </Fragment>
+    );
+};
+
+export default MyOpinions;
