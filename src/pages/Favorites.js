b18983a3a491d9d010b8eb49a5a8c4f5e7a29cd3
andresfelipedev <andresfelipedev@gmail.com>
2021-03-08 11:16:18 -0500

    Add Favorites.js

---
+++ src/pages/Favorites.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import OpinionsContainer from '../components/OpinionsContainer';
+
+const Favorites = () => {    
+
+    const { session, favoriteOpinions, favoriteOpinionsPageCount, favoriteOpinionsCurrentCount, favoriteOpinionsTotalCount } = useSelector(state => ({
+        session: state.UserReducer.session,
+        favoriteOpinions: state.OpinionReducer.favoriteOpinions,
+        favoriteOpinionsPageCount: state.OpinionReducer.favoriteOpinionsPageCount,
+        favoriteOpinionsCurrentCount: state.OpinionReducer.favoriteOpinionsCurrentCount,
+        favoriteOpinionsTotalCount: state.OpinionReducer.favoriteOpinionsTotalCount
+    }));
+    
+    const props = {
+        initialMessage: 'These are the opinions you like the most.',
+        opinions: favoriteOpinions,
+        pageCount: favoriteOpinionsPageCount,
+        currentCount: favoriteOpinionsCurrentCount,
+        totalCount: favoriteOpinionsTotalCount,
+        type: 'favorites'
+    };
+
+    if (!session) return <Redirect to={{ pathname: '/' }} />
+
+    return (
+        <Fragment>
+            <Header />
+            <OpinionsContainer {...props} />
+            <Footer />
+        </Fragment>
+    );
+};
+
+export default Favorites;
