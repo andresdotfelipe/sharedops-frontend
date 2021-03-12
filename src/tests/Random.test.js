ce48119ad76f36ac6fcb55e161e3ab6dad1d3112
andresfelipedev <andresfelipedev@gmail.com>
2021-03-12 12:50:42 -0500

    Create Random.test.js

---
+++ src/tests/Random.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import configureStore from 'redux-mock-store';
+import * as reactRedux from 'react-redux';
+import { shallow } from 'enzyme';
+import { getOpinion } from '../actions/opinions';
+import Random from '../pages/Random';
+
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+  
+    return {
+      ...originalModule,
+      useHistory: jest.fn(() => {
+        return { push: jest.fn(), location : { pathname: '/random' } };
+      }),
+      useRouteMatch: jest.fn(() => {
+        return { url: '/entry' };
+      }),
+    };
+});
+
+describe('Random component', () => {
+    const mockStore = configureStore();    
+    let store, wrapper;
+
+    store = mockStore({});
+    jest
+        .spyOn(reactRedux, 'useSelector')
+        .mockImplementation(state => store.getState());
+    jest
+        .spyOn(reactRedux, 'useDispatch')
+        .mockImplementation(() => store.dispatch);
+    store.clearActions();
+    store.dispatch = jest.fn();
+    wrapper = shallow(<Random />);    
+
+    it('has a button that dispatches a new random opinion on click', () => {                       
+        wrapper.find('Button.random').simulate('click');
+        expect(store.dispatch).toHaveBeenCalledWith(getOpinion('random'));
+    });
+
+});
