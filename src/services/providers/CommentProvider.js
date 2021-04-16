40f957cc7b581214f663749946a4bebcecc13c64
andresfelipedev <andresfelipedev@gmail.com>
2021-04-16 13:35:46 -0500

    Remove getComments

--- src/services/providers/CommentProvider.js
+++ src/services/providers/CommentProvider.js
@@ -4,10 +4,6 @@ const baseURL = 'comments';
 
 export default class CommentProvider {
 
-    static getComments(opinionId) {
-        return HttpProvider.get(`${baseURL}?opinionId=${opinionId}`).then(response => response.data);
-    }
-
     static createComment(data) {
         return HttpProvider.post(`${baseURL}`, data).then(response => response.data);
     }    
