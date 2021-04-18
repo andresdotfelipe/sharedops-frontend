8bcef6cf28ac19fbde7ce012040c1db3bfa30ba8
andresfelipedev <andresfelipedev@gmail.com>
2021-04-18 15:06:27 -0500

    Remove CommentReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,14 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import { UserReducer } from './UserReducer';
 import { OpinionReducer } from './OpinionReducer';
-import { CommentReducer } from './CommentReducer';
-import { actionTypes } from '../config/actionTypes';
 
 const mainReducer = combineReducers({
     form,
     UserReducer,
-    OpinionReducer,
-    CommentReducer
+    OpinionReducer
 });
 
 export default mainReducer;
