a17720919d555c63960972861cbd03ccd769dbc5
andresfelipedev <andresfelipedev@gmail.com>
2021-04-18 15:07:00 -0500

    Remove set comments and get comments

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,15 +1,5 @@
 import { actionTypes } from '../config/actionTypes';
 
-export const setComments = comments => ({
-    type: actionTypes.SET_COMMENTS,
-    comments
-});
-
-export const getComments = opinionId => ({
-    type: actionTypes.GET_COMMENTS,
-    opinionId
-});
-
 export const createComment = data => ({
     type: actionTypes.CREATE_COMMENT,
     data
